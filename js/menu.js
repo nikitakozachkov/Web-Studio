3116786f315140e8b26e99abdb7d4abdc899d036
Nikita Kozachkov <kozachkovngoat@gmail.com>
2022-11-24 18:25:07 +0200

    upload js files

---
+++ js/menu.js
@@ -0,0 +1,27 @@
+(() => {
+  const mobileMenu = document.querySelector('[data-menu]');
+  const openMenuBtn = document.querySelector('[data-menu-close]');
+  const closeMenuBtn = document.querySelector('[data-menu-open]');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
